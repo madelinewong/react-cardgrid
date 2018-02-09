0798d45bbabc3a503b91a3d0d2dfe25a9d61d526
Madeline Wong <madelinegwong@outlook.com>
2018-02-08 19:02:21 -0500

    created the basic state object to represent our data

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor(){
+    super(); //this makes sure that the app becomes a component.
+    this.state = {
+      cards: [
+        {
+          src: "https://via.placeholder.com/250x250",
+          caption: "some things and stuff",
+          isLiked: true
+        }, 
+        {
+          src: "https://via.placeholder.com/250x250",
+          caption: "other things and stuff",
+          isLiked: false
+        }
+      ]
+    }
+  }
   render() {
+    console.log(this.state);
     return (
-   <h1>hello!</h1>
+   <div className="container">
+      <h1>CardGrid</h1>
+
+   </div>
     );
   }
 }
